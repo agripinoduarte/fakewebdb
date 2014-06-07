97d5f678487a1d46fec743f4b04cac94a7daf119
José Agripino Duarte da Silva <agripinoduarte@gmail.com>
2014-06-07 13:21:17 -0400

    adding mongosoup uri

--- models/loader.js
+++ models/loader.js
@@ -1,9 +1,10 @@
 ///// Model Loader
 // Loads Mongoose (MongoDB Mapper) and app models 
+mongodb_uri = "mongodb://trIMZYtzoXqd:tRCjpCHrQbge@dbs004.mongosoup.de/cc_trIMZYtzoXqd"
 
 global.mongoose = require('mongoose');
 global.Schema = require('mongoose/lib').Schema;
-global.db = mongoose.createConnection('mongodb://localhost/' + database);
+global.db = mongoose.createConnection(mongodb_uri + '/' + database);
 
 // Models
 module.exports.Source = require('./source');
