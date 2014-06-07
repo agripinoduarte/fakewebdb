a7608d6c585d45ef67cd61f9eeacf637f77d0154
José Agripino Duarte da Silva <agripinoduarte@gmail.com>
2014-06-07 13:29:46 -0400

    moving api/getsources to index.js

--- routes/index.js
+++ routes/index.js
@@ -45,4 +45,22 @@ router.post('/submit', function(req, res) {
    
 });
 
+router.get('/api/getSources',  function(req, res) {
+    if (req.param('limit') == undefined) {
+        limit = 40;
+    }
+    
+    query = models.Source.find({}).limit(limit);
+
+    query.sort('title');
+    query.select('title');
+    query.select('date');
+    query.select('url');
+
+    query.exec(function (err, results) {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
 module.exports = router;
